d4792e08aa1bed0df96a8c14b8575751a9f00d9d
aisyah <aaiisyah2007@gmail.com>
2025-03-03 15:43:01 +0700

    tambahan fitur GUI

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,16 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nama = textBox1.Text;
+            labelOutput.Text = "Halo " + nama;
+        }
     }
 }
